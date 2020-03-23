db00771c17dc7d09137a440e98cead34fc7c2828
Paul Biberstein <biberstein.paul@gmail.com>
2020-03-22 20:03:06 -0400

    Added setup page

--- frontend/src/stores/SetupPageStore.ts
+++ frontend/src/stores/SetupPageStore.ts
@@ -1,10 +1,107 @@
 import { computed, observable, action } from "mobx";
 
+const DEBUG = false;
 export class SetupPageStore {
 
     constructor(initializer: Partial<SetupPageStore>) {
         Object.assign(this, initializer);
+        let url = new URL(window.location.href)
+        let code = url.searchParams.get("code")
+        this.name = url.searchParams.get("name") as string
+        if (code == null) {
+            let characters = "qwertyuiopasdfghjklzxcvbnm123456789"
+            this.code = "" + characters[Math.floor(Math.random() * 35)] + characters[Math.floor(Math.random() * 35)] + characters[Math.floor(Math.random() * 35)] + characters[Math.floor(Math.random() * 35)]
+        } else {
+            this.code = code
+
+        }
+
+        if (DEBUG) {
+            this.gameState = {
+                players: [{name: "paul"}, {name: "zack"}, {name: "logan"}, {name: "harry"}, {name: "benny"}],
+                characters: []
+            }
+            return
+        }
+        console.log(`ws://${url.hostname}:8080/data?code=${this.code}`)
+        this.socket = new WebSocket(`ws://${url.hostname}:8080/data?code=${this.code}`)
+        this.socket.onopen = (event) => {
+            this.sendMessage({
+                message_type: "control",
+                message_subtype: "introduction",
+                data: this.name
+            })
+        }
+        this.socket.onmessage = (event) => {
+            let message = JSON.parse(event.data);
+            if (message.message_type == "game_state_update") {
+                this.gameState = message.data;
+            }
+            if (this.gameState.state === "game") {
+                let newUrl = new URL(window.location.href)
+                newUrl.pathname = '/game'
+                newUrl.search = `name=${this.name}&code=${this.code}`
+                window.location.href = newUrl.href
+            }
+            console.log("Recieved initial game state")
+            console.log(message)
+            this.isLoading = false;
+        } 
     }
 
+    readonly socket: WebSocket;
+
+    @observable
+    public name: string;
+
+    @observable
+    public code: string;
+
+    @observable
+    public gameState: any = {}
+
+    @observable
+    public isLoading: boolean = true;
+
+    readonly characters: string[][] = [
+        [ "Doppelganger", "Werewolf 1", "Werewolf 2", "Minion", "Mason 1", "Mason 2", "Seer", "Robber", "Troublemaker", "Drunk", "Insomniac", "Villager 1", "Villager 2", "Villager 3", "Hunter", "Tanner" ],
+        [ "Sentinel", "Alpha Wolf", "Mystic Wolf", "Apprentice Seer", "P.I", "Witch", "Village Idiot", "Revealer", "Curator", "Dream Wolf", "Bodyguard"],
+        ["Oracle", "Alien 1", "Alien 2", "Synthetic Alien", "Cow", "Groob", "Zerb", "Leader", "Psychic", "Rascal", "Exposer", "Blob", "Mortician"]
+    ]
+    readonly characterGroupLabels: string[] = [
+        "Werewolf",
+        "Daybreak",
+        "Alien"
+    ]
+
+    @action
+    toggleCharacter(characterName: string) {
+        if (this.gameState.characters_enabled.flat().includes(characterName)) {
+            this.sendMessage({
+                message_type: "character_select",
+                action_type: "remove",
+                data: characterName
+            })
+        } else {
+            this.sendMessage({
+                message_type: "character_select",
+                action_type: "add",
+                data: characterName
+            })
+        }
+    }
+
+    @action
+    beginGame() {
+        this.sendMessage({
+            message_type: "control",
+            message_subtype: "begin_game"
+        })
+    }
+
+    @action
+    sendMessage(message: any) {
+        this.socket.send(JSON.stringify(message))
+    }
 
 }
--- frontend/src/views/SetupPageView.tsx
+++ frontend/src/views/SetupPageView.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
 
+import Button from 'react-bootstrap/Button';
+
+
 import { SetupPageStore } from "../stores/SetupPageStore"
 
 import "../styles/SetupPageStyle.scss"
@@ -14,12 +17,44 @@ export class SetupPageView extends React.Component<IProps> {
 
     render() {
         let store = this.props.store
-        return (
-            <div className="body">
+        if (store.isLoading) {
+            return <div>loading</div>
+        } else {
+            return (
                 <div className="setup-page">
-
+                    <h1>Code: <em>{store.code}</em></h1>
+                    <div className="players">
+                        <h1>Players</h1>
+                        {store.gameState.players.map((player: any) => 
+                            <div className="player">{player.name}</div>
+                        )}
+                    </div>
+                    <h1>Characters</h1>
+                    <div className="characters">
+                        {store.characters.map((characterSet, i) => 
+                            <div className="character-set" key={store.characterGroupLabels[i]}>
+                                <h2>{store.characterGroupLabels[i]}</h2>
+    
+                                <div className="character-tile-grid">
+                                    {characterSet.map(characterName =>
+                                        <div className="character-tile" key={characterName} onClick={(() => store.toggleCharacter(characterName))}>
+                                            <div>{characterName}</div>
+                                            <div>{store.gameState.characters_enabled.includes(characterName) ? "☒" : "☐"} </div>
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                    <Button
+                        disabled={store.gameState.players.length !== store.gameState.characters_enabled.length - 3}
+                        onClick={store.beginGame.bind(store)}
+                        variant="primary">
+                            Begin Game
+                    </Button>
                 </div>
-            </div>
-        )
+            )
+        }
+        
     }
 }

309c5164b1f16d360874b120a5607992d101a4f5
Paul Biberstein <biberstein.paul@gmail.com>
2020-03-24 15:38:17 -0400

    Added disclaimer and header

--- frontend/src/views/WelcomePageView.tsx
+++ frontend/src/views/WelcomePageView.tsx
@@ -26,6 +26,10 @@ export class WelcomePageView extends React.Component<IProps> {
                     <Button disabled={store.name.length === 0} onClick={store.newGame.bind(store)} variant="primary">New Game</Button>
                     <Button disabled={!store.name || store.code.length !== 4} onClick={store.joinGame.bind(store)} variant="primary">Join Game</Button>
                 </span>
+                <div className="spacer" />
+                <div className="disclaimer">
+                    This is a fan made online version of the <a href="https://beziergames.com/collections/one-night-ultimate-werewolf">One Night games by Bezier Games</a>. Please go support them and don't play this unless you own the game in real life.
+                </div>
             </div>
         )
     }
